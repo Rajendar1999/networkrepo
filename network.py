a6fa463f39596ec7548336878cdc3617942a1a8c
Rajendar <rajendar1999@gmail.com>
2026-09-24 20:02:21 +0530

    added a change

--- network.py
+++ network.py
@@ -1,5 +1,5 @@
 def multiply(a, b):
-	"""Return the product of two numbers."""
+	"""Return the product of two numbers. i have modified some changes"""
 	return a * b
 
 print(multiply(3, 4))  # Output: 12
